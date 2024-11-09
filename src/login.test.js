d6ff78f4a19115d94163fcbd2c867c5fc14fb84d
JoshWilbur <joshuawilbur03@gmail.com>
2024-11-09 14:42:19 -0500

    add basic test for video demo

--- src/login.test.js
+++ src/login.test.js
@@ -1,9 +1,10 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import Login from './login';
 import { MemoryRouter } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 describe('Login Component', () => {
-  test('renders email and password inputs', () => {
+  test('Confirm input fields are rendered', () => {
     render(
       <MemoryRouter>
         <Login authorized_true={() => {}} />
@@ -17,4 +18,25 @@ describe('Login Component', () => {
     // Check if the password input is in the document
     const passwordInput = screen.getByPlaceholderText('Password');
     expect(passwordInput).toBeInTheDocument();
-  })});
+
+    // Check that login with Google is rendered
+    const loginGoogle = screen.getByText('Login with Google')
+    expect(loginGoogle).toBeInTheDocument();
+  })},
+
+  test('Test email/password input', () => {
+    render(
+      <MemoryRouter>
+        <Login authorized_true={() => {}} />
+      </MemoryRouter>
+    );
+
+    const emailInput = screen.getByPlaceholderText('Email');
+    const passwordInput = screen.getByPlaceholderText('Password');
+
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+    expect(emailInput.value).toBe('test@example.com');
+    expect(passwordInput.value).toBe('password123');
+  }));
