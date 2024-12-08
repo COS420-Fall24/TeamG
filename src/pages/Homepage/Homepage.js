9fa8f8e49e6c56f1c532ea995743dbee321f3db4
LAPTOP-AL8JKVBA\saura <saurav.pokhrel@maine.edu>
2024-12-08 02:42:22 -0500

    TransactionHistoryReincorporated

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header';
 import BudgetDashboard from '../../components/BudgetDashboard';
 import ActionButtons from '../../components/ActionButtons';
@@ -9,8 +9,9 @@ import {
   UpdateCategoryModal, 
   NewCategoryModal, 
   IncomeModal, 
-  DeleteAccountModal 
- } from '../../components/Modals';
+  DeleteAccountModal,
+  TransactionHistoryModal
+} from '../../components/Modals';
 import { useAuth } from '../../hooks/useAuth';
 import { useBudgetData } from '../../hooks/useBudgetData';
 import { useTutorial } from '../../hooks/useTutorial';
@@ -19,19 +20,26 @@ import './Homepage.css';
 const Homepage = () => {
   const [activeModal, setActiveModal] = useState(null);
   const { user, handleLogout, handleDeleteAccount } = useAuth();
-  const { catData, catLabels, income, handleFormSubmit, handleClearData } = useBudgetData(user?.uid);
-  const {tutorial, currentStep, handleTutorialActions } = useTutorial(user?.uid);
+  const { 
+    catData, 
+    catLabels, 
+    income, 
+    transactions,
+    handleFormSubmit, 
+    handleClearData 
+  } = useBudgetData(user?.uid);
+  const { tutorial, currentStep, handleTutorialActions } = useTutorial(user?.uid);
 
   return (
     <div className="homepage-container">
-      {<Header />}
+      <Header />
 
       {tutorial && (
         <TutorialOverlay 
           currentStep={currentStep}
           onAction={handleTutorialActions}
         />
-      )} 
+      )}
       
       <div className="content">
         <BudgetDashboard 
@@ -42,41 +50,50 @@ const Homepage = () => {
         <ActionButtons onActionClick={setActiveModal} />
       </div>
 
-      {/* Modal components */}
       <LogTransactionModal 
         show={activeModal === 'log'}
         onClose={() => setActiveModal(null)}
         onSubmit={handleFormSubmit}
         categories={catLabels}
       />
+
       <UpdateCategoryModal 
         show={activeModal === 'update'}
         onClose={() => setActiveModal(null)}
         onSubmit={handleFormSubmit}
         categories={catLabels}
       />
+
       <NewCategoryModal 
         show={activeModal === 'new'}
         onClose={() => setActiveModal(null)}
         onSubmit={handleFormSubmit}
       />
+
       <IncomeModal 
         show={activeModal === 'income'}
         onClose={() => setActiveModal(null)}
         onSubmit={handleFormSubmit}
       />
+
       <DeleteAccountModal 
         show={activeModal === 'deleteAccount'}
         onClose={() => setActiveModal(null)}
         onConfirm={handleDeleteAccount}
       />
 
-      { <Footer 
+      <TransactionHistoryModal
+        show={activeModal === 'transactionHistory'}
+        onClose={() => setActiveModal(null)}
+        transactions={transactions}
+      />
+
+      <Footer 
         onTutorialClick={handleTutorialActions.start}
         onLogoutClick={handleLogout}
         onDeleteAccountClick={() => setActiveModal('deleteAccount')}
         onClearData={handleClearData}
-      /> }
+      />
     </div>
   );
 };
